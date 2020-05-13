9dc6026bc018b36b857e96737c8835f49064c899
Shawn Mahon <shawn.mahon@kuka.com>
2020-05-13 16:19:30 -0500

    Learning nested components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
-import CommentDetail from './CommentDetail';
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-        <CommentDetail author="Sam" />
-        <CommentDetail author="Alex" />
-        <CommentDetail author="Jess" />
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo="Today at 4:45PM"
+          message="Test1"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo="Today at 2:00AM"
+          message="Test2"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo="Yesterday at 5:45PM"
+          message="Test3"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
