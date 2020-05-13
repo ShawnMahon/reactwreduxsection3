f0b58395d5ca7cb47b115d574647f78cab02c73c
Shawn Mahon <shawn.mahon@kuka.com>
2020-05-13 15:10:32 -0500

    Comments template

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const App = () => {
+    return <div>Hi there!</div>
+
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'))
