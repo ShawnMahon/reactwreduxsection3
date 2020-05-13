22ecbaa9108049258748f5f591224766f8eb56c5
Shawn Mahon <shawn.mahon@kuka.com>
2020-05-13 15:29:54 -0500

    Comments using components

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-
-const App = () => {
-    return <div>Hi there!</div>
-
-};
-
-ReactDOM.render(<App />, document.querySelector('#root'))
